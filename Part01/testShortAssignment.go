50a7b1e9adf220c8c98f8bb900d7581184d2b138
Suho Kwon <34533493+Knix008@users.noreply.github.com>
2022-05-19 12:07:13 +0900

    Add files via upload

---
+++ Part01/testShortAssignment.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	variable01 := 1
+	variable01 := 2
+	fmt.Println(variable01)
+}
---
+++ Part01/testShortAssignment02.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	i, k := 3, 4
+	j, k := 1, 2
+	fmt.Println(i, j, k)
+}
