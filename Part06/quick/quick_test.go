2ef62386107055f89eb30d355d29acb1f06b5e1c
Suho Kwon <suho.kwon@gmail.com>
2022-07-13 16:56:33 +0900

    Add modified testinig/quick package example.

--- Part06/quick/quick_test.go
+++ Part06/quick/quick_test.go
@@ -16,6 +16,7 @@ func TestWithSystem(t *testing.T) {
 		t.Errorf("Error: %v", err)
 	}
 }
+
 func TestWithItself(t *testing.T) {
 	condition := func(a, b uint16) bool {
 		return Add(a, b) == Add(b, a)
